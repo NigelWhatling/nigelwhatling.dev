4a685f721b2ceafba3308e2fd363e50881aa04f2
Nigel Whatling <nigelw@n-vision.com.au>
2019-08-20 23:26:37 +1000

    Comment out a gatsby-source-filesystem plugin.

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,13 +15,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `posts`,
-        path: `${__dirname}/src/posts`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `posts`,
+    //     path: `${__dirname}/src/posts`,
+    //   },
+    // },
     `gatsby-plugin-sass`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
